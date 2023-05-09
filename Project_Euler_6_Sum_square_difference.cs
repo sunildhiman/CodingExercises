7873601aa7ff1713ff5783993efbe9d645f39601
sunil Dhiman <sunil_k_dhiman@outlook.com>
2023-05-10 00:51:31 +0100

    Update Project_Euler_6_Sum_square_difference.cs
    corrected

--- Project_Euler_6_Sum_square_difference.cs
+++ Project_Euler_6_Sum_square_difference.cs
@@ -1,22 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Numerics;
+class Solution {
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Reps");
+    static void Main(String[] args) {
         int t = Convert.ToInt32(Console.ReadLine());
-        for (int a0 = 0; a0 < t; a0++)
-        {
-            Console.WriteLine("Enter the number");
+        for(int a0 = 0; a0 < t; a0++){
             int n = Convert.ToInt32(Console.ReadLine());
-            List<int> numbers = Enumerable.Range(1, n).ToList();
-            long sum = numbers.Sum();
-            BigInteger sumOfSquares = numbers.Select(x => x * x).Sum();
-            BigInteger result = sum*sum - sumOfSquares;
+           List<int> numbers = Enumerable.Range(1, n).ToList();
+            BigInteger sum = numbers.Sum();
+            BigInteger sumOfSquares = numbers.Select(x => (long)x * x).Sum();
+            BigInteger result =  sum*sum - (numbers.Select(x => (long)x * x).Sum());
             Console.WriteLine(result);
-
         }
     }
-
 }
